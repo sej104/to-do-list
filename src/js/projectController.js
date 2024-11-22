a53a726ee44f3da22afc450da115ee36e577fc4d
Sergio Joya <sejoya104@gmail.com>
2024-11-22 11:58:33 -0500

    Add project functionalities

---
+++ src/js/projectController.js
@@ -0,0 +1,28 @@
+import Project from "./Project.js";
+
+const projects = [];
+
+const getProjects = () => projects;
+
+const addProject = (name) => {
+    projects.push(new Project(name));
+}
+
+const deleteProject = (index) => {
+    projects.splice(index, 1);
+}
+
+const printProjects = () => {
+    console.log('Projects:')
+    for (let project of projects) {
+        console.log(`${project.title}`);
+    }
+    console.log('');
+}
+
+export {
+    getProjects, 
+    addProject,
+    deleteProject,
+    printProjects 
+};
